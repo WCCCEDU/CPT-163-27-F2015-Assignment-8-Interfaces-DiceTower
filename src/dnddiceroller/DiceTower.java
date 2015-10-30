e213d2e75f1000bc1b73171e81f7aa97c219686b
froschs <froschs@my.wccc.edu>
2015-10-30 14:16:20 -0400

    Assignment 8 Commit 1
    First commit for assignment 8 interface project. Don't know what changes
    to make to the driver to test the new dice configurations.

--- src/dnddiceroller/DiceTower.java
+++ src/dnddiceroller/DiceTower.java
@@ -13,16 +13,56 @@ import java.util.List;
  * A dice tower will accept a collection of dice and reports their results when
  * they reach the tray at the bottom
  * @author Paul Scarrone
+ * @author Seth Frosch
  */
 public class DiceTower {
-  final int PANEL_COUNT = 3;
-  List<Die> dice;
+  private final int PANEL_COUNT = 3;
+  private List<NumberedDie> numberedDice;
+  private List<Fudge> fudgeDice;
+  private List<Loaded> loadedDice;
+  private int trayValue; //Accumulator to total value of dice rolls
 
   public DiceTower() {
-	this.dice = new ArrayList();
+	this.numberedDice = new ArrayList();
+        this.fudgeDice = new ArrayList();
+        this.loadedDice = new ArrayList();
   }
   
   public DiceTower(List dice) {
-	this.dice = dice;
+	this.numberedDice = numberedDice;
+        this.fudgeDice = fudgeDice;
+        this.loadedDice = loadedDice;
   }
+  
+  //Accessor to get the total value for dice rolls
+  public int getTrayValue(){
+      return trayValue;
+  }
+  
+  /**
+   * @param dropDice method rolls each die in the arraylist for each panel and
+   * the accumulator totals those values
+   */
+  public void dropDice(){
+      for(int i = 0; i < this.numberedDice.size(); i++){
+          for(int ii = 0; ii < this.PANEL_COUNT; ii++){
+              this.numberedDice.get(i).roll();
+          }
+          this.trayValue += this.numberedDice.get(i).getValue();
+      }
+      for(int i = 0; i < this.fudgeDice.size(); i++){
+          for(int ii = 0; ii < this.PANEL_COUNT; ii++){
+              this.fudgeDice.get(i).roll();
+          }
+          this.trayValue += this.fudgeDice.get(i).getValue();
+      }
+      for(int i = 0; i < this.loadedDice.size(); i++){
+          for(int ii = 0; ii < this.PANEL_COUNT; ii++){
+              this.loadedDice.get(i).roll();
+          }
+          this.trayValue += this.loadedDice.get(i).getValue();
+      }
+  }
+
+    
 }
--- src/dnddiceroller/DnDDiceRoller.java
+++ src/dnddiceroller/DnDDiceRoller.java
@@ -6,6 +6,7 @@ import java.util.List;
 /**
  *
  * @author Paul Scarrone
+ * @author Seth Frosch
  */
 public class DnDDiceRoller {
 
@@ -27,9 +28,9 @@ public class DnDDiceRoller {
   }
   
   public static int test_oneD6(){
-	Die die = new Die(6);
+	NumberedDie die = new NumberedDie(6);
 	die.roll();
-	int dieValue = die.value();
+	int dieValue = die.getValue();
 	if(dieValue >= 1 && dieValue <= 6){
 	  return -1; // Means the die value is outside its bounds for a d6
 	}else{
@@ -38,12 +39,12 @@ public class DnDDiceRoller {
   }
   
   public static int test_diceTowerWithTwoD6(){
-	List<Die> dice = new ArrayList();
-	dice.add(new Die(6));
-	dice.add(new Die(6));
-	DiceTower tower = DiceTower(dice);
+	List<NumberedDie> dice = new ArrayList();
+	dice.add(new NumberedDie(6));
+	dice.add(new NumberedDie(6));
+	DiceTower tower = new DiceTower(dice);
 	tower.dropDice();
-	int trayValue = tower.trayValue();
+	int trayValue = tower.getTrayValue();
 	if(trayValue >= 2 && trayValue <= 12){
 	  return -1; // means the die value is outside the bounds of 2 d6
 	}else{
