7a629cff80db087ac87e9786942cc2e3f7ff3646
Samantha Shurie <s_shurie@yahoo.com>
2015-11-04 14:21:34 -0500

    Assignment 8 Interfaces
    I'm not sure if I did all of this correctly, but it seemed to run okay
    when I tested it. Any help would be much appreciated!

---
+++ Assignment8Interfaces/src/assignment8interfaces/Die.java
@@ -0,0 +1,15 @@
+
+package assignment8interfaces;
+
+public interface Die {
+  /**
+   * Roll method simulates rolling of the die
+   */
+  public void roll();
+  
+  /**
+   * 
+   * @return integer value of die 
+   */
+  public int getValue();
+}
