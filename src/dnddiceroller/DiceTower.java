4324e825a49a469e748afb454d0578736ecdb47a
DTkach <DTkach3@hotmail.com>
2015-11-03 19:13:27 -0500

    Coding Commit
    It works IN THEORY, which is the best way to work.

--- src/dnddiceroller/DiceTower.java
+++ src/dnddiceroller/DiceTower.java
@@ -15,14 +15,62 @@ import java.util.List;
  * @author Paul Scarrone
  */
 public class DiceTower {
-  final int PANEL_COUNT = 3;
-  List<Die> dice;
+        private final int PANEL_COUNT = 3;
+        private List<NumberedDie> numberedDice;
+        private List<Fudge> fudgeDice;
+        private List<Loaded> loadedDice;
+        private int trayValue;
 
   public DiceTower() {
-	this.dice = new ArrayList();
+	this.numberedDice = new ArrayList();
+        this.fudgeDice = new ArrayList();
+        this.loadedDice = new ArrayList();
+        trayValue = 0;
   }
   
   public DiceTower(List dice) {
-	this.dice = dice;
+	this.numberedDice = numberedDice;
+        this.fudgeDice = fudgeDice;
+        this.loadedDice = loadedDice;
+        trayValue = 0;
   }
+
+  public int getTrayValue() {
+        return trayValue;
+  }
+  
+  
+  /**
+   * Utilizes the roll() method per the number of panels in the dice tower.
+   */
+  public void dropDice() {
+        int i = 0;
+        for (NumberedDie die : numberedDice){
+            for(int x = 0; x < PANEL_COUNT; x++){
+                numberedDice.get(i).roll();
+            }
+            trayValue += numberedDice.get(i).getValue();
+            i++;
+        }
+        
+        int v = 0;
+        for (Fudge die : fudgeDice){
+            for(int x = 0; x < PANEL_COUNT; x++){
+                numberedDice.get(v).roll();
+            }
+            trayValue += numberedDice.get(v).getValue();
+            v++;
+        }
+        
+        int z = 0;
+        for (Loaded die : loadedDice){
+            for(int x = 0; x < PANEL_COUNT; x++){
+                numberedDice.get(z).roll();
+            }
+            trayValue += numberedDice.get(z).getValue();
+            z++;
+        }
+  }
+  
+  
 }
